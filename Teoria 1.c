5bb8af8032a03f7d0b602fab152b47927bb60d0e
Caio893 <caio_DBB@hotmail.com>
2024-04-08 18:25:28 -0700

    aulas

---
+++ Teoria 1.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //Biblioteca para "interface I/O" para usuários.
+#include <stdlib.h> //Alternativa para sistemas Unix usar o stdio.io//getchar().
+int main(void){
+    //código
+    printf("Hello, World!\n");
+
+    getchar();
+}
---
+++ Teoria 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("Mensagem 1\n"); 
+    printf("Mensagem 2\n"); 
+    printf("Mensagem 3\n"); 
+    printf("Mensagem 4\n"); 
+    printf("Mensagem 5\n");
+    return 0;
+
+}
+
+//Debug no VS não funcionou e precisa arrumar usar o Dev.C++ para fazer o debug.
